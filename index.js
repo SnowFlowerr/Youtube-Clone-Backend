0bebaad4ac9461a5f43c5c7a92e15e06233c7fe4
SnowFlowerr <bhudeokrit@gmail.com>
2024-07-25 13:36:16 +0530

    versel.json added

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ const connectApp=()=>{
         throw error;
     })
 }
-app.use('/',(req,res)=>{
-    return res.json("running")
-})
+
 app.use('/api/users',userRoutes)
 app.use('/api/videos',videoRoutes)
 app.use('/api/auth',authRoutes)
