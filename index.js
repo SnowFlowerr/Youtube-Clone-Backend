24a190704a3ab55f784e0b18f1514a080c5ec2c6
SnowFlowerr <bhudeokrit@gmail.com>
2024-08-24 09:21:19 +0530

    bug fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://video-streaming-app-frontend-lilac.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
